3e14f26336e41715d00087697c5e67b70780dcd4
Andrin <andrin@4maag.ch>
2026-02-25 00:51:09 +0100

    chore: bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.andrin.examcountdown"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.3.3"
+        versionCode = 15
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
